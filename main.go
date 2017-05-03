43f67a1f75df4af21df89975a197e3e772eaa759
Romain Hardy <romain.hardy17@gmail.com>
2017-05-03 15:20:49 +0000

    Add Gin Gonic router
    Create endpoint to trigger sync to load DB

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"os"
+
+	"github.com/gin-gonic/gin"
 	"github.com/romain-h/juddes/gists"
 )
 
 func main() {
-	gists.Sync()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.GET("/sync-all", func(c *gin.Context) {
+		gists.Sync()
+	})
+
+	r.Run(":" + os.Getenv("PORT"))
 }
