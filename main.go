cdeb83abd575aa333e26a9a3f2beffb5424e7044
Romain Hardy <romain.hardy17@gmail.com>
2017-05-03 16:09:39 +0000

    Implement dumb authorization middleware
    Since this API will be used by one client where we can store a hardcoded
    token, we can simply match on Authorization header

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/romain-h/juddes/gists"
 )
 
+var authorizationHeader = fmt.Sprintf("Bearer %s", os.Getenv("JUDDES_ACCESS_TOKEN"))
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Header.Get("Authorization") != authorizationHeader {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			c.Abort()
+		}
+	}
+}
+
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 
-	r.GET("/sync-all", func(c *gin.Context) {
+	authorized := r.Group("/", Auth()) // Apply Auth middleware
+
+	authorized.GET("/sync-all", func(c *gin.Context) {
 		gists.Sync()
 	})
 
